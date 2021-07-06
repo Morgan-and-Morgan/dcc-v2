29229713897ffc304b6a0d3d3a84123d649910a1
Roman Dyn <roman@classaction.com>
2021-07-06 21:35:09 +0300

    added js fix

--- web/themes/custom/dcc/js/custom.js
+++ web/themes/custom/dcc/js/custom.js
@@ -1,7 +1,7 @@
 const $ = jQuery;
 
 $(document).ready(function ($) {
-  $('body.toggled .region-we-mega-menu nav .we-mega-menu-ul li[data-level="0"]>a.we-mega-menu-li').click(function (event) {
+  $('.block-we-megamenu-blockheader-navigation .region-we-mega-menu nav .we-mega-menu-ul li[data-level="0"]>a.we-mega-menu-li').click(function (event) {
     $(this).once().trigger("click");
   });
 });
