306640cacfc7dd03415c6cffeafb41d8a7f42e81
mykola.bolshakov <mykola.bolshakov@gmail.com>
2024-04-10 00:33:32 +0300

    WEM-5062 | Added type2 banner. Styled.

--- web/modules/custom/dcc_general/src/Plugin/Block/BannerBlock.php
+++ web/modules/custom/dcc_general/src/Plugin/Block/BannerBlock.php
@@ -21,10 +21,10 @@ class BannerBlock extends BlockBase {
   public function build() {
     return [
       '#content' => [
-        'banner_title' => \Drupal::config('dcc_general.banner_settings')->get('banner_title') ?? '',
-        'banner_description' => \Drupal::config('dcc_general.banner_settings')->get('banner_description') ?? '',
-        'banner_button_title' => \Drupal::config('dcc_general.banner_settings')->get('banner_button_title') ?? '',
-        'banner_button_link' => \Drupal::config('dcc_general.banner_settings')->get('banner_button_link') ?? '',
+        'banner_title' => \Drupal::config('dcc_general.banner_settings')->get('banner_title') ?: '',
+        'banner_description' => \Drupal::config('dcc_general.banner_settings')->get('banner_description') ?: '',
+        'banner_button_title' => \Drupal::config('dcc_general.banner_settings')->get('banner_button_title') ?: '',
+        'banner_button_link' => \Drupal::config('dcc_general.banner_settings')->get('banner_button_link') ?: '',
       ],
       '#theme' => 'dcc_general_banner_block',
     ];
