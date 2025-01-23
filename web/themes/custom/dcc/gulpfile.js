6ccbfcf6a3d18a99fa67c356dca1ce29041662a6
mykola.bolshakov <mykola.bolshakov@gmail.com>
2025-01-23 03:37:19 +0200

    2025-01-r1 | Modify theme build process.

--- web/themes/custom/dcc/gulpfile.js
+++ web/themes/custom/dcc/gulpfile.js
@@ -1,74 +1,49 @@
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
 const sourcemaps = require("gulp-sourcemaps");
 const webpack = require("webpack-stream");
 
 /** SASS TASKS **/
-gulp.task("sass:build", function (cb) {
-  gulp
-    .src("./scss/style.scss", "./scss/custom/slick.scss")
-    // .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }).on("error", sass.logError))
-    // .pipe(sourcemaps.write())
-    // .pipe(concat('index.css'))
+gulp.task("sass:build", function () {
+  return gulp
+    .src(["./scss/style.scss", "./scss/custom/slick.scss"]) // Use an array for multiple files
+    // .pipe(sourcemaps.init()) // Uncomment if sourcemaps are required
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    // .pipe(sourcemaps.write("./maps")) // Sourcemaps directory
     .pipe(gulp.dest("./dist/css"));
-  cb();
 });
 
-gulp.task("sass:watch", function (cb) {
-  gulp.watch(["./scss/style.scss", "./scss/*/*.scss"], gulp.series("sass:build"));
-  cb();
+gulp.task("sass:watch", function () {
+  return gulp.watch(["./scss/**/*.scss"], gulp.series("sass:build")); // Optimized glob pattern
 });
 
 /** JS TASKS **/
-gulp.task("js:build-main-js", function (cb) {
-  gulp
+gulp.task("js:build-main-js", function () {
+  return gulp
     .src("./js/main.js")
     .pipe(
       webpack({
-        //config options
         entry: "./js/main.js",
-        output: {
-          filename: "main-min.js"
-        },
+        output: { filename: "main-min.js" },
         mode: "production",
         optimization: {
           minimize: true,
-        }
+        },
       })
     )
     .pipe(gulp.dest("./dist/js"));
-  cb();
 });
 
-/* watch js directory for changes */
-gulp.task("js:watch", function (cb) {
-  gulp.watch(
-    "./js/*.js",
-    gulp.series(
-      "js:build-main-js"
-    )
-  );
-  cb();
+gulp.task("js:watch", function () {
+  return gulp.watch("./js/**/*.js", gulp.series("js:build-main-js")); // Optimized glob pattern
 });
 
 /** ASSETS WATCH **/
 gulp.task(
   "assets:watch",
-  gulp.series(
-    "sass:build",
-    "sass:watch",
-    "js:build-main-js",
-    "js:watch"
-  )
+  gulp.series("sass:build", gulp.parallel("sass:watch", "js:watch"))
 );
 
 /** ASSETS BUILD **/
-gulp.task(
-  "assets:build",
-  gulp.series(
-    "sass:build",
-    "js:build-main-js",
-  )
-);
+gulp.task("assets:build", gulp.series("sass:build", "js:build-main-js"));
